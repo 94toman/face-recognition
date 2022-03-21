fe8c977419eb3a86c53a466468a4a1afda68ed5a
94toman <94toman@gmail.com>
2022-03-21 18:01:17 +0100

    initial push

---
+++ src/App.js
@@ -0,0 +1,99 @@
+import React, { component } from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import Rank from './components/Rank/Rank';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Particles from 'react-tsparticles';
+
+function App() {
+  return (
+    <div className="App">
+      <Particles className='particles'
+         id="tsparticles"
+         options={particlesOptions}
+       />
+
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+
+      {/* <FaceRecognition />    } */}
+    </div>
+  );
+}
+
+export default App;
+
+const particlesOptions = 
+  {
+    fpsLimit: 120,
+
+    particles: {
+      number: {
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+        value: 60,
+      },
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 300,
+        enable: true,
+        opacity: 0.5,
+        width: 1,
+      },
+      collisions: {
+        enable: true,
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outMode: "bounce",
+        random: false,
+        speed: 1.5,
+        straight: false,
+      },
+
+      opacity: {
+        value: 0.5,
+      },
+      shape: {
+        type: "circle",
+      },
+    },
+    detectRetina: true,
+    interactivity: {
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push",
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse",
+        },
+        resize: true,
+      },
+      modes: {
+        bubble: {
+          distance: 150,
+          duration: 2,
+          opacity: 0.8,
+          size: 40,
+        },
+        push: {
+          quantity: 2,
+        },
+        repulse: {
+          distance: 150,
+          duration: 0.4,
+        },
+      },
+    },
+  }
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Tilt from 'react-parallax-tilt';
+import './Logo.css';
+import face from './face-recognition.png'
+
+const Logo = () =>{
+    return (
+        <div className="ma4 mt0 w4">
+            <Tilt className="Tilt br2 shadow-2" tiltMaxAngleX={50} tiltMaxAngleY={50}>
+                <div className="pa3">
+                    <img src={face} alt='logo'/>
+                </div>                    
+            </Tilt>
+        </div>
+       
+    )
+}
+
+export default Logo;
